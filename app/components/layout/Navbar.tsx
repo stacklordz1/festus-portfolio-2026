055cbab98853db996b6969064cb0a03a9ba28051
stacklordz1 <chidubemoffiaukwu2@gmail.com>
2026-08-10 19:01:37 +0100

    working on navbar

--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -1,8 +1,33 @@
-
+import { navigation } from "@/app/lib/navigation"
+import { ArrowRight } from "lucide-react"
+'s enough for
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="glass flex items-center justify-between fixed top-6 left-1/2 -translate-x-1/2 rounded-2xl">
+      {/* avatar/logo */}
+      <div>
+        {/* <Image src='/favicon.ico' width={32} height={32} alt="Festus"/> */}
+      </div>
+
+      {/* nav content */}
+      <div>
+        {navigation.map((item)=>(
+          <a href={item.href} key={item.href}
+          className="">
+            {item.title}
+          </a>
+        ))}
+      </div>
+
+      {/* resume */}
+      <div>
+        <a href="" download>
+          Resume
+        </a>
+        <ArrowRight/>
+      </div>
+    </nav>
   )
 }
 
